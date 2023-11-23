6a1c5ec58d1d7b1bbc3c379ee94b34ebf46e6bf0
Luca Manasek <luca.manasek@student.htldornbirn.at>
2023-11-23 09:14:19 +0100

    01_JS_finished

---
+++ JS_Basics/01_JS/script.js
@@ -0,0 +1,37 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+
+let result1 = a + b + c;
+console.log(result1);
+let result2 = (a+ b) / c;
+console.log(result2);
+let result3 = c - a + b;
+console.log(result3);
+
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+let result4 = a + b + c;
+console.log(result4);
+let result5 = (a+ b) / c;
+console.log(result5);
+let result6 = c - a + b;
+console.log(result6);
