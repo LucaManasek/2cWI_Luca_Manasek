68a31b2ba38d9a91e38790e5731635f3348f9233
Luca Manasek <luca.manasek@student.htldornbirn.at>
2023-11-30 08:43:45 +0100

    04_JS_finished

---
+++ JS_Basics/04_JS/script.js
@@ -0,0 +1,37 @@
+// Zähle von 7 bis 45
+// Zähle von 9 bis 99
+// Zähle rückwärts von 10 bis 0
+// Zähle rückwärts von 1234 bis 1207
+
+// Gib Deinen Namen genau 8 mal aus
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+
+for (let i = 7; i < 46; i++) {
+    console.log(i);
+}
+
+for (let i = 9; i < 100; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+for (let i = 1234; i > 1206; i--) {
+    console.log(i);
+}
+
+for (let i = 0; i < 8; i++) {
+    console.log("Luca Manasek");
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    if (i == 10)
+    console.log("Luca Manasek");
+}
+
+for (let i = 10; i >=0; i--) {
+    console.log(i);
+}
