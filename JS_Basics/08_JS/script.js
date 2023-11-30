1ab489e3b198ce037c3b32153752eb3058450f8f
Luca Manasek <luca.manasek@student.htldornbirn.at>
2023-11-30 09:29:46 +0100

    end_of_lesson

---
+++ JS_Basics/08_JS/script.js
@@ -0,0 +1,10 @@
+ // Erstelle eine Zufallszahl zwischen 5 und 10
+
+// Wenn der Wert 10 ist gib aus Ten
+// Wenn der Wert 9 ist gib aus Nine
+// Wenn der Wert 8 ist gib aus Eight
+// etc.
+
+let randomNumber = Math.round(Math.random)
+
+if (randmumNumber = 10)
