6b591ba907f568aa827267d1607b3c7432014dfd
Luca Manasek <luca.manasek@student.htldornbirn.at>
2023-11-30 09:00:25 +0100

    05_JS_finished

---
+++ JS_Basics/05_JS/script.js
@@ -0,0 +1,39 @@
+// erstelle eine Variable number
+
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large
+
+let number = 55;
+let number2 = 75;
+if (number < 20) {
+    console.log("Mini");
+}
+
+else if (number <= 50) {
+    console.log("Medium");
+}
+
+else if (number > 50) {
+    console.log("Large");
+}
+
+if (number2 <= 7) {
+    console.log("Mini");
+}
+
+else if (number2 <= 90) {
+    console.log("Medium");
+}
+
+else if (number2 > 90) {
+    console.log("Large");
+}
