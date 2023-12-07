35c2692b2b068154a0b3951a8c7e627972255dc8
Luca Manasek <luca.manasek@student.htldornbirn.at>
2023-12-07 08:33:54 +0100

    08_JS_finished

--- JS_Basics/08_JS/script.js
+++ JS_Basics/08_JS/script.js
@@ -5,6 +5,27 @@
 // Wenn der Wert 8 ist gib aus Eight
 // etc.
 
-let randomNumber = Math.round(Math.random)
+let randomNumber = Math.round(Math.random() * (10-5) +5);
 
-if (randmumNumber = 10)
+console.log(randomNumber);
+
+switch(randomNumber) {
+    case 10:
+        console.log("Ten");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+}
